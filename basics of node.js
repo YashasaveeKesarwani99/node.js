7acd8ad8b00e7a03ab016531634a723e34174523
YashasaveeKesarwani99 <56859852+YashasaveeKesarwani99@users.noreply.github.com>
2020-10-13 21:40:31 +0530

    Create basic of node.js
    1. fs
    2. http/https

---
+++ basics of node.js
@@ -0,0 +1,10 @@
+Node.js is a javascript runtime enviroment. Here we'll focus on writing backend in javascript using node.js
+
+    1. fs - One of the core modules of node.js used to perform functions on file
+        1. writeFileSync() - to make a file and write data into it.
+        2. 
+    2. http - again, a core module of node.js used to create a server and work in it
+        1. createServer() - as the name suggests
+        2. write()
+        3. end() - to mark the end of process.
+        
